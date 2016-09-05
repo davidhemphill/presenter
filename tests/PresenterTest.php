5961077398e4ba00b7c26844ab7b15d3bb45c8a2
David Hemphill <davidlee.hemphill@gmail.com>
2016-09-04 23:39:17 -0500

    Convert to using Eloquent for tests

--- tests/PresenterTest.php
+++ tests/PresenterTest.php
@@ -2,13 +2,38 @@
 
 use Hemp\Presenter;
 use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Capsule\Manager;
 
 class PresenterTest extends PHPUnit_Framework_TestCase
 {
-    public function setUp()
+    function setupDatabase()
     {
-        parent::setUp();
+        $capsule = new Manager;
 
+        $capsule->addConnection([
+            'driver'    => 'sqlite',
+            'host'      => 'localhost',
+            'database'  => ':memory:',
+            'username'  => '',
+            'password'  => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]);
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+
+        Manager::schema()->create('test_models', function($table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->timestamps();
+        });
+    }
+
+    function registerCollectionMacro()
+    {
         Collection::macro('present', function ($class) {
             return $this->map(function ($object) use ($class) {
                 return present($object, $class);
@@ -16,17 +41,32 @@ class PresenterTest extends PHPUnit_Framework_TestCase
         });
     }
 
+    function setUp()
+    {
+        parent::setUp();
+        $this->setUpDatabase();
+        $this->registerCollectionMacro();
+    }
+
+    function createModel()
+    {
+        return new TestModel([
+            'first_name' => 'David',
+            'last_name' => 'Hemphill',
+        ]);
+    }
+
     /** @test */
     function it_decorates_objects()
     {
-        $sampleModel = new SampleModel;
+        $sampleModel = $this->createModel();
         $presenter = new SamplePresenter($sampleModel);
     }
 
     /** @test */
     function you_can_get_the_decorated_model()
     {
-        $sampleModel = new SampleModel;
+        $sampleModel = $this->createModel();
         $presenter = new SamplePresenter($sampleModel);
 
         $this->assertSame($sampleModel, $presenter->getModel());
@@ -35,7 +75,7 @@ class PresenterTest extends PHPUnit_Framework_TestCase
     /** @test */
     function it_can_have_its_own_methods()
     {
-        $sampleModel = new SampleModel;
+        $sampleModel = $this->createModel();
         $presenter = new SamplePresenter($sampleModel);
 
         $this->assertEquals('David Hemphill', $presenter->name());
@@ -44,7 +84,7 @@ class PresenterTest extends PHPUnit_Framework_TestCase
     /** @test */
     function it_can_call_the_decorated_objects_methods()
     {
-        $sampleModel = new SampleModel;
+        $sampleModel = $this->createModel();
         $presenter = new SamplePresenter($sampleModel);
 
         $this->assertEquals(90210, $presenter->timeStamp());
@@ -53,7 +93,7 @@ class PresenterTest extends PHPUnit_Framework_TestCase
     /** @test */
     function it_can_return_the_decorated_objects_properties()
     {
-        $sampleModel = new SampleModel;
+        $sampleModel = $this->createModel();
         $presenter = new SamplePresenter($sampleModel);
 
         $this->assertEquals('David', $presenter->first_name);
@@ -62,7 +102,7 @@ class PresenterTest extends PHPUnit_Framework_TestCase
     /** @test */
     function it_can_overload_the_decorated_objects_methods()
     {
-        $sampleModel = new SampleModel;
+        $sampleModel = $this->createModel();
         $presenter = new SamplePresenter($sampleModel);
 
         $this->assertEquals('This is your decorator speaking', $presenter->overloadedMethod());
@@ -71,7 +111,7 @@ class PresenterTest extends PHPUnit_Framework_TestCase
     /** @test */
     function it_can_have_its_own_magic_properties()
     {
-        $sampleModel = new SampleModel;
+        $sampleModel = $this->createModel();
         $presenter = new SamplePresenter($sampleModel);
 
         $this->assertEquals('David Lee Hemphill', $presenter->full_name);
@@ -80,7 +120,7 @@ class PresenterTest extends PHPUnit_Framework_TestCase
     /** @test */
     function you_can_wrap_a_collection_of_eloquent_models()
     {
-        $sampleModel = new SampleModel;
+        $sampleModel = $this->createModel();
 
         $users = collect([$sampleModel])->present(SamplePresenter::class);
 
@@ -92,9 +132,10 @@ class PresenterTest extends PHPUnit_Framework_TestCase
     }
 }
 
-class SampleModel {
-    public $first_name = 'David';
-    public $last_name = 'Hemphill';
+class TestModel extends Model
+{
+    protected $guarded = [];
+
     public function overloadedMethod()
     {
         return 'This is the original method!';
