8f1776789be074870086806297acc347749f1c42
Marcus Moore <contact@marcusmoore.io>
2017-03-17 15:26:41 -0700

    present via closure

--- helpers.php
+++ helpers.php
@@ -1,14 +1,20 @@
 <?php
 
+use Hemp\Presenter\EmptyModel;
+
 if (!function_exists('present')) {
     /**
      * Helper to Present Models
      * @param  string $model
-     * @param  string $presenter
+     * @param  \Closure|string $presentationStrategy
      * @return object
      */
-    function present($model, $presenter)
+    function present($model, $presentationStrategy)
     {
-        return new $presenter($model);
+        if ($presentationStrategy instanceof \Closure) {
+            return new EmptyModel($presentationStrategy($model));
+        }
+
+        return new $presentationStrategy($model);
     }
 }
---
+++ src/EmptyModel.php
@@ -0,0 +1,12 @@
+<?php namespace Hemp\Presenter;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmptyModel extends Model
+{
+    /**
+     * Allow any attribute to be set.
+     * @var array
+     */
+    protected $guarded = [];
+}
